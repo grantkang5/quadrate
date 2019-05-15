1957c327bce4b008ede629031f3dda2ef5abe97b
grantkang5 <g1k05j@gmail.com>
2019-05-14 18:35:03 -0700

    fix import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const path = require('path')
 const app = express();
-import path from 'path'
 const port = process.env.PORT || 3050;
 
 app.use(express.static(path.join(__dirname, 'dist')))
