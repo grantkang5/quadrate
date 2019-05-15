0cb16117aa8c8aced9fc3e6d32d6c253a9dc77e0
grantkang5 <g1k05j@gmail.com>
2019-05-14 18:42:28 -0700

    change herokus cripts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = process.env.PORT || 3050;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.get('*', (req, res) => {
